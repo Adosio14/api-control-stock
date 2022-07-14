471c9d4c9553b1e27f5518080e6b5583ee936f9d
Lucas Barbero <lucas95_273@hotmail.com>
2022-07-14 20:32:45 -0300

    Pruebas

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -1,5 +1,24 @@
 import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import Categories from "../types/categories";
+
+const repository = getRepository(Categories);
 
 export const getAllCategories = async (req: Request, res: Response) => {
   res.send("Categories");
+};
+export const getCategoryById = async (req: Request, res: Response) => {
+  res.send("Category x by ID");
+};
+
+export const createCategory = async (req: Request, res: Response) => {
+  const category = new Categories();
+
+  category.name = req.body.name;
+  try {
+    await repository.save(category);
+    res.status(200).send("Categoría creada");
+  } catch (error) {
+    res.status(404).send(error);
+  }
 };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -4,3 +4,5 @@ import { CategoriesControllers } from "../controllers";
 export const categoriesRoutes = Router();
 
 categoriesRoutes.get("/", CategoriesControllers.getAllCategories);
+categoriesRoutes.get("/:id", CategoriesControllers.getCategoryById);
+categoriesRoutes.post("/", CategoriesControllers.createCategory);
