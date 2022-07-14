7066b1e12a4a9985f147745f8c8400da63518036
Lucas Barbero <lucas95_273@hotmail.com>
2022-07-14 20:15:27 -0300

    Conexion a la BD

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import bodyParser from "body-parser";
+import { createConnection } from "typeorm";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -10,5 +11,9 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+createConnection("default")
+  .then(() => console.log("Conectado a la BD"))
+  .catch((err) => console.log(err));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
